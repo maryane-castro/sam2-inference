c8a75c41d6e015b6deea09b7f3e223fef3520b67
Maryane Castro <maryane.castro993@gmail.com>
2025-01-16 21:27:27 -0300

    update

--- roboflow/download_weigths/export_and_inference.py
+++ roboflow/download_weigths/export_and_inference.py
@@ -1,31 +1,29 @@
 import os
 import cv2
-import numpy as np
+import inference
+import supervision as sv
 import matplotlib.pyplot as plt
 
+
 os.environ["MODEL_CACHE_DIR"] = "./models_cache"
-from inference.models.utils import get_model
 model_id = "<id_model>"
-model = get_model(model_id=model_id, api_key="<api_key>")
+model = inference.get_model(model_id, api_key="<api_key>")
 
 image_path = "<path_image>"
-
-# inference
-results = model.infer(image_path)[0]
-
 image = cv2.imread(image_path)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-for prediction in results.predictions:  
-    points = np.array([(int(point.x), int(point.y)) for point in prediction.points])  
-    cv2.polylines(image, [points], isClosed=True, color=(0, 255, 0), thickness=2) 
-    cv2.fillPoly(image, [points], color=(0, 255, 0, 50))
+results = model.infer(image)[0]
+detections = sv.Detections.from_inference(results)
 
-    x, y = points[0]
-    label = f"{prediction.class_name} ({prediction.confidence:.2f})"
-    cv2.putText(image, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+mask_annotator = sv.MaskAnnotator()
+label_annotator = sv.LabelAnnotator()
+annotated_image = mask_annotator.annotate(scene=image, detections=detections)
+annotated_image = label_annotator.annotate(scene=annotated_image, detections=detections)
 
-plt.figure(figsize=(10, 6))
-plt.imshow(image)
-plt.axis("off")
+annotated_image_rgb = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(10, 10))
+plt.imshow(annotated_image_rgb)
+plt.axis('off')  
+plt.title("Annotated Image")
 plt.show()
